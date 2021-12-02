aa23bca364fa8b7ee0c6af32cacd7c85b9f0d7dc
Juan <1987jvm@gmail.com>
2021-12-02 17:08:15 +0100

    Juan2

---
+++ Schedule/src/Schedule/Schedule.java
@@ -0,0 +1,21 @@
+package Schedule;
+
+import Course.Courses;
+
+import java.util.ArrayList;
+
+public class Schedule {
+    private String id;
+    private String semester;
+    private String className;
+    private ArrayList<Courses>courses;
+
+    public Schedule(String id,String semester,String className,ArrayList<Courses>courses ){
+        this.id=id;
+        this.semester=semester;
+        this.className=className;
+        this.courses=new ArrayList<>();
+
+    }
+    
+}
